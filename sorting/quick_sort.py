eb06b4642e2b88515b769a6cd2517e8fa8ecd5ae
Atishay Jain <j1atishay@gmail.com>
2020-06-07 11:42:31 +0530

    Added Quick Sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,40 @@
+"""
+"""
+def print_status(lower: list, pivot: int, upper: list) -> None:
+	print("Pivot:", pivot)
+	print("Lower:", *lower)
+	print("Upper:", *upper)
+	print()
+
+
+def quick_sort(arr: list) -> list:
+	if len(arr) < 2:
+		return arr
+
+	else:
+		mid = len(arr) // 2
+		pivot = arr[mid]
+		lower = list()
+		higher = list()
+
+		for i in range(len(arr)):
+			if arr[i] < pivot:
+				lower.append(arr[i])
+			elif arr[i] > pivot:
+				higher.append(arr[i])
+			elif i != mid:
+				lower.append(arr[i])
+
+		# For showing working of algorithm
+		print_status(lower, pivot, higher)
+
+		return quick_sort(lower) + [pivot] + quick_sort(higher)
+
+
+arr = [2, 1, 5, 1, 3, 9, 1 , 8, 13]
+print("Given Array:")
+print(*arr)
+print()
+arr = quick_sort(arr)
+print("Sorted Array:")
+print(*arr)
